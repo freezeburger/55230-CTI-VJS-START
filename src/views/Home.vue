741c59f4bf509b0b3496e66e3bdbf2b0fd8b5168
freezeburger <renaud.dubuis@decryptage.net>
2025-05-28 11:24:17 +0200

    Update

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import * as Layouts from '@/components/layouts'
+</script>
+
+<template>
+    <Layouts.Default title="Home">
+        
+        <div>
+            Home Content
+        </div>
+
+    </Layouts.Default>
+</template>
